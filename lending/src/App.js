e72ac5d99a2e6ff7f512ddbaa29237b57e1ee55c
vladimir.jovanovic@getpenta.com <jovanovicv90@gmail.com>
2019-10-20 14:55:19 +0200

    Push

--- lending/src/App.js
+++ lending/src/App.js
@@ -4,7 +4,7 @@ import {
   Switch,
   Route,
 } from "react-router-dom";
-import { RequestLoan } from "./pages/RequestLoan";
+import RequestLoan from "./pages/RequestLoan";
 import './App.scss';
 
 function App() {
--- lending/src/pages/RequestLoan.js
+++ lending/src/pages/RequestLoan.js
@@ -1,11 +1,136 @@
-import React from "react";
+import React, { useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-export const RequestLoan = () => {
+const useStyles = makeStyles(theme => ({
+    '@global': {
+        body: {
+            backgroundColor: theme.palette.common.white,
+        },
+    },
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+export default function RequestLoan() {
+    const classes = useStyles();
+
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [amount, setAmount] = useState('');
+    const [toAddress, setToAddress] = useState('');
+
+    const submitForm = (e) => {
+        e.preventDefault();
+        console.log(title, description, amount, toAddress);
+        alert(`Request for ${title} in an amount of ${amount} ETH  is sent to ${toAddress}`)
+        setTitle("")
+        setDescription("")
+        setAmount("")
+        setToAddress("")
+    }
 
 
     return (
-        <div>
-            Request loan
-        </div>
-    )
-};
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Typography component="h1" variant="h5">
+                    Request a Loan
+                </Typography>
+                <form className={classes.form} noValidate>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                autoComplete="title"
+                                name="title"
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="title"
+                                label="Title"
+                                autoFocus
+                                value={title}
+                                onChange={(e)=> setTitle(e.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                multiline={true}
+                                id="description"
+                                label="Description"
+                                name="description"
+                                value={description}
+                                onChange={(e)=> setDescription(e.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                multiline={true}
+                                id="amount"
+                                label="Amount"
+                                name="amount"
+                                value={amount}
+                                onChange={(e)=> setAmount(e.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                multiline={true}
+                                id="toAddress"
+                                label="To address"
+                                name="toAddress"
+                                value={toAddress}
+                                onChange={(e)=> setToAddress(e.target.value)}
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                        onClick={(e)=> submitForm(e)}
+                    >
+                        Submit request
+                    </Button>
+                </form>
+            </div>
+        </Container>
+    );
+}
